5a0d7fcc7e2069a13c842e44422467ce117cc4f5
logmi <lgm8573@163.com>
2021-08-30 23:37:08 +0800

    enter

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex;">
+        <div style="width: 200px">花店管理系统</div>
+        <div style="flex: 1"></div>
+        <div style="width: 100px">下拉框</div>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        name: 'Header',
+    }
+</script>
